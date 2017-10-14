bb354a39471dbedcf74809b6e75f08f0e4dcfc78
Jonathan Chaney <jonchaney@gmail.com>
2017-10-14 13:13:41 -0700

    mobile styling

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -9,7 +9,7 @@ class About extends React.Component {
   render() {
     return (
       <div className="about">
-        <p>The goal of this site is to create a searchable database of flyers from diy shows in the San Francisco Bay Area.</p>
+        <p>The goal of this site is to create a searchable and updatable database of diy shows in the San Francisco Bay Area.</p>
       </div>
     );
   }
