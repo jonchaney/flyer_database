a08da09371c87dcc41a5cb36a1085e1ea9d4bf04
Jonathan Chaney <jonchaney@gmail.com>
2017-10-21 10:44:06 -0700

    events model

--- app/controllers/api/bands_controller.rb
+++ app/controllers/api/bands_controller.rb
@@ -1,2 +1,44 @@
 class Api::BandsController < ApplicationController
+  def show
+    @band = Band.find(params[:id])
+  end
+
+  def create
+    @band = Band.new(band_params)
+    if @band.save
+      render 'api/bands/show'
+    else
+      render json: @band.errors.full_messages, status: 422
+    end
+  end
+
+  def index
+    @bands = Band.all
+    render :index
+  end
+
+  def update
+    @band = Band.find(params[:id])
+    if @band.update_attributes(band_params)
+      render :show
+    else
+      render json: @band.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @band = Band.find(params[:id])
+
+    if @band.destroy
+      render json: ['success']
+    else
+      render json: ['error'], status: 404
+    end
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name)
+  end
 end
