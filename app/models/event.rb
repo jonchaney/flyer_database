57eb6683a8596eb2005f37fa1733f02c94727dd1
Andrew Booth <booth@shipt.com>
2017-10-22 23:04:06 -0700

    Run annotate

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id                   :integer          not null, primary key
+#  price                :integer
+#  date                 :string           not null
+#  presented_by         :string
+#  poster_creator       :string
+#  venue                :string
+#  city                 :string
+#  address              :string
+#  image_file_name      :string
+#  image_content_type   :string
+#  image_file_size      :integer
+#  image_updated_at     :datetime
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  poster_transcription :text
+#
+
 class Event < ApplicationRecord
   validates :date, presence: true
   has_and_belongs_to_many :bands
